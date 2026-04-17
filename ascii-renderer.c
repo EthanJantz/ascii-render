599a32f0bcd9d2e3f2522e5cd38d719a7bbdefa4
EthanJantz <21095848+EthanJantz@users.noreply.github.com>
2026-04-17 18:26:23 -0500

    functionalize conversion process

--- ascii-renderer.c
+++ ascii-renderer.c
@@ -5,13 +5,59 @@
 #include "stb_image.h"
 
 char getCharFromLightness(float lightness);
+char *convertToASCII(unsigned char *img, int width, int height, int channels,
+                     int samples, int cols, int rows);
 
 char ASCII[10] = {' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'};
 int N_ASCII = sizeof(ASCII) / sizeof(ASCII[0]);
+int BLOCK_WIDTH = 8;
+int BLOCK_HEIGHT = 16;
 
 char getCharFromLightness(float lightness) {
   int idx = floor(lightness * (N_ASCII - 1));
   return ASCII[idx];
+}
+
+char *convertToASCII(unsigned char *img, int width, int height, int channels,
+                     int samples, int cols, int rows) {
+  size_t out_size = cols * rows + rows;
+  char *out = malloc(out_size);
+
+  unsigned char r, g, b;
+  float rel_lum;
+  float avg_rel_lum;
+
+  for (int row = 0; row < rows; row++) {
+    for (int col = 0; col < cols; col++) {
+      for (int ch = 0; ch < channels; ch++) {
+        avg_rel_lum = 0.0;
+
+        for (int s = 1; s <= samples; s++) {
+          int x = col * BLOCK_WIDTH + BLOCK_WIDTH / s;
+          int y = row * BLOCK_HEIGHT + BLOCK_HEIGHT / s;
+
+          if (ch == 0) {
+            r = img[(y * width + x) * 3 + ch];
+          } else if (ch == 1) {
+            g = img[(y * width + x) * 3 + ch];
+          } else {
+            b = img[(y * width + x) * 3 + ch];
+            rel_lum = ((r * 0.2126) + (g * 0.7152) + (b * 0.0722)) / 255;
+            avg_rel_lum += rel_lum;
+          }
+        }
+      }
+      avg_rel_lum /= samples;
+      // printf("Sampled Relative luminance: %f\n", avg_rel_lum);
+
+      out[row * (cols + 1) + col] = getCharFromLightness(avg_rel_lum);
+
+      if (col == cols - 1) {
+        out[row * (cols + 1) + cols] = '\n';
+      }
+    }
+  }
+  return out;
 }
 
 int main(int argc, char *argv[]) {
@@ -30,50 +76,11 @@ int main(int argc, char *argv[]) {
 
   printf("Loaded: %dx%d, %d channels\n", width, height, channels);
 
-  int block_width = 8;
-  int block_height = 16;
-  int cols = width / block_width;
-  int rows = height / block_height;
+  int cols = width / BLOCK_WIDTH;
+  int rows = height / BLOCK_HEIGHT;
+  int samples = 8;
 
-  int samples = 4;
-
-  size_t out_size = cols * rows + rows;
-  char *out = malloc(out_size);
-
-  unsigned char r, g, b;
-  float rel_lum;
-  float avg_rel_lum;
-
-  for (int row = 0; row < rows; row++) {
-    for (int col = 0; col < cols; col++) {
-      for (int ch = 0; ch < channels; ch++) {
-        avg_rel_lum = 0.0;
-
-        for (int s = 1; s <= samples; s++) {
-          int x = col * block_width + block_width / s;
-          int y = row * block_height + block_height / s;
-
-          if (ch == 0) {
-            r = img[(y * width + x) * 3 + ch];
-          } else if (ch == 1) {
-            g = img[(y * width + x) * 3 + ch];
-          } else {
-            b = img[(y * width + x) * 3 + ch];
-            rel_lum = ((r * 0.2126) + (g * 0.7152) + (b * 0.0722)) / 255;
-            avg_rel_lum += rel_lum;
-          }
-        }
-      }
-      avg_rel_lum /= samples;
-      printf("Sampled Relative luminance: %f\n", avg_rel_lum);
-
-      out[row * (cols + 1) + col] = getCharFromLightness(avg_rel_lum);
-
-      if (col == cols - 1) {
-        out[row * (cols + 1) + cols] = '\n';
-      }
-    }
-  }
+  char *out = convertToASCII(img, width, height, channels, samples, cols, rows);
 
   FILE *fo = fopen(argv[2], "wb");
   if (fo == NULL) {
@@ -82,6 +89,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  size_t out_size = cols * rows + cols;
   size_t bytes_written = fwrite(out, 1, out_size, fo);
   printf("Wrote %zu bytes\n", bytes_written);
   fclose(fo);
