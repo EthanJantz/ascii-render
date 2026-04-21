0b45b280c4b6df6380dd74f0c4637cf0fa828cd4
EthanJantz <21095848+EthanJantz@users.noreply.github.com>
2026-04-21 16:03:27 -0500

    MVP ncurses ascii renderer tui
    does what it says on the tin

--- ascii-renderer-nc.c
+++ ascii-renderer-nc.c
@@ -27,43 +27,231 @@ typedef struct {
   int len;
 } UserInput;
 
-int readImg(char *filename, Image *img);
-ASCIIRender generateASCIIRender(char *out, size_t out_size, int cols, int rows);
-char getCharFromLightness(float lightness);
-ASCIIRender convertToASCII(Image *img, int block_width, int samples);
-void updateInput(UserInput *cur, int in);
+typedef struct {
+  int term_w, term_h;
+  WINDOW *input_win, *ascii_win;
+
+  Image img;
+  ASCIIRender cur_render;
+  UserInput user_input;
+
+  char error[256];
+  bool should_rerender;
+  bool should_quit;
+} AppState;
+
+void setup_ncurses();
+AppState init_state();
+void update(AppState *state, int event);
+void render(AppState *state);
+void teardown(AppState *state);
+void reinit_windows(AppState *state);
+int read_img(char *filename, Image *img);
+ASCIIRender generate_ascii_render(char *out, size_t out_size, int cols,
+                                  int rows);
+char get_char_from_lightness(float lightness);
+ASCIIRender convert_to_ascii(Image *img, int block_width, int samples);
+void update_input(UserInput *cur, int in);
 
 char ASCII[10] = {' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'};
 int N_ASCII = sizeof(ASCII) / sizeof(ASCII[0]);
 int BLOCK_ASPECT_RATIO = 2;
 int SAMPLES = 1;
 
-int readImg(char *filename, Image *img) {
+void setup_ncurses() {
+  srand(time(0));
+  set_escdelay(25);
+  initscr();
+  curs_set(0);
+  cbreak();
+  noecho();
+  keypad(stdscr, TRUE);
+  refresh();
+}
+
+AppState init_state() {
+  int sx, sy;
+  getmaxyx(stdscr, sy, sx);
+
+  int input_w, input_h, input_x, input_y, ascii_w, ascii_h, ascii_x, ascii_y,
+      margin;
+  margin = 1; // 1 char
+  input_w = sx - (margin * 2);
+  input_h = 3;
+  input_x = margin;
+  input_y = margin;
+
+  ascii_w = sx - (margin * 2);
+  ascii_h = sy * .9 - (margin * 2);
+  ascii_x = margin;
+  ascii_y = input_y + input_h;
+
+  WINDOW *input_win = newwin(input_h, input_w, input_y, input_x);
+  wborder(input_win, 0, 0, 0, 0, 0, 0, 0, 0);
+  mvwprintw(input_win, 1, 1, "pos: %dx%d size: %dwx%dh", input_x, input_y,
+            input_w, input_h);
+
+  WINDOW *ascii_win = newwin(ascii_h, ascii_w, ascii_y, ascii_x);
+  wborder(ascii_win, 0, 0, 0, 0, 0, 0, 0, 0);
+  mvwprintw(ascii_win, 1, 1, "pos: %dx%d size: %dwx%dh", ascii_x, ascii_y,
+            ascii_w, ascii_h);
+
+  AppState state = {.term_w = sx,
+                    .term_h = sy,
+                    .input_win = input_win,
+                    .ascii_win = ascii_win,
+                    .should_rerender = true,
+                    .should_quit = false};
+
+  return state;
+}
+
+void update(AppState *state, int event) {
+
+  switch (event) {
+  case 27:
+    state->should_quit = 1;
+    return;
+  case '\n':
+  case '\r':
+    state->should_rerender = 1;
+    return;
+  default:
+    update_input(&state->user_input, event);
+    state->should_rerender = 1;
+    break;
+  }
+
+  if (state->should_rerender && state->user_input.len > 0) {
+    int err = read_img(state->user_input.buf, &state->img);
+    if (err) {
+      mvwprintw(state->ascii_win, 2, 1, "Could not load image\n");
+      return;
+    }
+
+    int ascii_w, ascii_h;
+    getmaxyx(state->ascii_win, ascii_h, ascii_w);
+    int interior_h = ascii_h - 2;
+    int interior_w = ascii_w - 2;
+    int denom_h = BLOCK_ASPECT_RATIO * interior_h;
+    // ceil idiom: ceil(x / y) == (x + y - 1) / y
+    int bw_w = (state->img.width + interior_w - 1) / interior_w;
+    int bw_h = (state->img.height + denom_h - 1) / denom_h;
+    int bw = bw_h > bw_w ? bw_h : bw_w;
+    if (bw == 0)
+      bw = 1;
+
+    state->cur_render = convert_to_ascii(&state->img, bw, SAMPLES);
+  }
+
+  return;
+}
+
+void render(AppState *state) {
+  if (!state->should_rerender)
+    return;
+
+  int ascii_w, ascii_h;
+  getmaxyx(state->ascii_win, ascii_h, ascii_w);
+  int interior_h = ascii_h - 2;
+  int interior_w = ascii_w - 2;
+
+  // center render if necessary
+  int offset_h = 1;
+  int offset_w = 1;
+  if (state->cur_render.cols < interior_w) {
+    offset_w = (interior_w - state->cur_render.cols) / 2;
+    offset_w = offset_w == 0 ? 1 : offset_w;
+  }
+
+  if (state->cur_render.rows < interior_h) {
+    offset_h = (interior_h - state->cur_render.rows) / 2;
+    offset_h = offset_h == 0 ? 1 : offset_h;
+  }
+
+  mvwprintw(state->input_win, 1, 1, "%s", state->user_input.buf);
+  mvwprintw(state->input_win, 1, 1, "%-*s", state->term_w - 4,
+            state->user_input.buf);
+  wrefresh(state->input_win);
+  for (int i = 0; i < state->cur_render.rows; i++) {
+    mvwaddnstr(state->ascii_win, offset_h + i, offset_w,
+               &state->cur_render.buf[i * (state->cur_render.cols + 1)],
+               state->cur_render.cols);
+  }
+  mvwprintw(state->ascii_win, 1, 1, "pos: %dx%d size: %dwx%dh", ascii_w,
+            ascii_h, ascii_w, ascii_h);
+  wrefresh(state->ascii_win);
+
+  mvwprintw(state->input_win, 1, 1, "%s", state->user_input.buf);
+  wrefresh(state->input_win);
+  state->should_rerender = 0;
+}
+
+void reinit_windows(AppState *state) {
+  int sx, sy;
+  getmaxyx(stdscr, sy, sx);
+
+  int input_w, input_h, input_x, input_y, ascii_w, ascii_h, ascii_x, ascii_y,
+      margin;
+  margin = 1; // 1 char
+  input_w = sx - (margin * 2);
+  input_h = 3;
+  input_x = margin;
+  input_y = margin;
+
+  ascii_w = sx - (margin * 2);
+  ascii_h = sy * .9 - (margin * 2);
+  ascii_x = margin;
+  ascii_y = input_y + input_h;
+
+  WINDOW *input_win = newwin(input_h, input_w, input_y, input_x);
+  wborder(input_win, 0, 0, 0, 0, 0, 0, 0, 0);
+  mvwprintw(input_win, 1, 1, "pos: %dx%d size: %dwx%dh", input_x, input_y,
+            input_w, input_h);
+
+  WINDOW *ascii_win = newwin(ascii_h, ascii_w, ascii_y, ascii_x);
+  wborder(ascii_win, 0, 0, 0, 0, 0, 0, 0, 0);
+  mvwprintw(ascii_win, 1, 1, "pos: %dx%d size: %dwx%dh", ascii_x, ascii_y,
+            ascii_w, ascii_h);
+
+  state->term_w = sx;
+  state->term_h = sy;
+  state->ascii_win = ascii_win;
+  state->input_win = input_win;
+}
+
+int read_img(char *filename, Image *img) {
   img->img = stbi_load(filename, &img->width, &img->height, &img->channels, 3);
   if (img->img == NULL) {
-    printf("Could not load image\n");
+    // printf("Could not load image\n");
     return 1;
   }
   return 0;
 }
 
-char getCharFromLightness(float lightness) {
+void teardown(AppState *state) {
+  free(state->cur_render.buf);
+  stbi_image_free(state->img.img);
+  endwin();
+}
+
+char get_char_from_lightness(float lightness) {
   int idx = floor(lightness * (N_ASCII - 1));
   return ASCII[idx];
 }
 
-ASCIIRender generateASCIIRender(char *out, size_t out_size, int cols,
-                                int rows) {
-  assert(out != NULL);
-  assert(rows > 0);
-  assert(cols > 0);
-  assert(out_size == rows * cols + rows);
+ASCIIRender generate_ascii_render(char *out, size_t out_size, int cols,
+                                  int rows) {
+  // assert(out != NULL);
+  // assert(rows > 0);
+  // assert(cols > 0);
+  // assert(out_size == rows * cols + rows);
   ASCIIRender render = {
       .buf = out, .rows = rows, .cols = cols, .size = out_size};
   return render;
 }
 
-ASCIIRender convertToASCII(Image *img, int block_width, int samples) {
+ASCIIRender convert_to_ascii(Image *img, int block_width, int samples) {
   unsigned char r, g, b;
   float rel_lum;
   float avg_rel_lum;
@@ -97,7 +285,7 @@ ASCIIRender convertToASCII(Image *img, int block_width, int samples) {
         }
       }
       avg_rel_lum /= samples;
-      out[row * (cols + 1) + col] = getCharFromLightness(avg_rel_lum);
+      out[row * (cols + 1) + col] = get_char_from_lightness(avg_rel_lum);
 
       if (col == cols - 1) {
         out[row * (cols + 1) + cols] = '\n';
@@ -105,11 +293,11 @@ ASCIIRender convertToASCII(Image *img, int block_width, int samples) {
     }
   }
 
-  ASCIIRender render = generateASCIIRender(out, out_size, cols, rows);
+  ASCIIRender render = generate_ascii_render(out, out_size, cols, rows);
   return render;
 }
 
-void updateInput(UserInput *cur, int in) {
+void update_input(UserInput *cur, int in) {
   if (in == KEY_BACKSPACE || in == 127 || in == 8) {
     if (cur->len == 0)
       return;
@@ -128,98 +316,12 @@ void updateInput(UserInput *cur, int in) {
 }
 
 int main() {
-  srand(time(0));
-  set_escdelay(25);
-  initscr();
-  curs_set(0);
-  cbreak();
-  noecho();
-  keypad(stdscr, TRUE);
-  refresh();
-
-  // term size
-  int sx, sy;
-  getmaxyx(stdscr, sy, sx);
-
-  // init windows
-  int input_w, input_h, input_x, input_y, ascii_w, ascii_h, ascii_x, ascii_y,
-      margin;
-  margin = 1; // 1 char
-  input_w = sx - (margin * 2);
-  input_h = 3;
-  input_x = margin;
-  input_y = margin;
-
-  ascii_w = sx - (margin * 2);
-  ascii_h = sy * .8 - (margin * 2);
-  ascii_x = margin;
-  ascii_y = input_y + input_h;
-
-  WINDOW *input_win = newwin(input_h, input_w, input_y, input_x);
-  wborder(input_win, 0, 0, 0, 0, 0, 0, 0, 0);
-  mvwprintw(input_win, 1, 1, "pos: %dx%d size: %dwx%dh", input_x, input_y,
-            input_w, input_h);
-
-  WINDOW *ascii_win = newwin(ascii_h, ascii_w, ascii_y, ascii_x);
-  wborder(ascii_win, 0, 0, 0, 0, 0, 0, 0, 0);
-  mvwprintw(ascii_win, 1, 1, "pos: %dx%d size: %dwx%dh", ascii_x, ascii_y,
-            ascii_w, ascii_h);
-
-  // init state
-  UserInput input = {"", 0};
-
-  // read image
-  Image img;
-  readImg("eigenface.png", &img);
-
-  // get render that fits within ascii_win
-  ASCIIRender render = {.buf = NULL, 0};
-  int interior_h = ascii_h - 2;
-  int interior_w = ascii_w - 2;
-  int denom_h = BLOCK_ASPECT_RATIO * interior_h;
-  // ceil idiom: ceil(x / y) == (x + y - 1) / y
-  int bw_w = (img.width + interior_w - 1) / interior_w;
-  int bw_h = (img.height + denom_h - 1) / denom_h;
-  int bw = bw_h > bw_w ? bw_h : bw_w;
-  if (bw == 0)
-    bw = 1;
-  render = convertToASCII(&img, bw, SAMPLES);
-
-  // center render if necessary
-  int offset_h = 1;
-  int offset_w = 1;
-  if (render.cols < interior_w) {
-    offset_w = (interior_w - render.cols) / 2;
-    offset_w = offset_w == 0 ? 1 : offset_w;
+  setup_ncurses();
+  AppState state = init_state();
+  while (!state.should_quit) {
+    render(&state);
+    int key = getch();
+    update(&state, key);
   }
-
-  if (render.rows < interior_h) {
-    offset_h = (interior_h - render.rows) / 2;
-    offset_h = offset_h == 0 ? 1 : offset_h;
-  }
-
-  // loop until user presses q
-  int ch = getch();
-  do {
-    if (ch == 27)
-      break;
-    updateInput(&input, ch);
-
-    wrefresh(input_win);
-    for (int i = 0; i < render.rows; i++) {
-      mvwaddnstr(ascii_win, offset_h + i, offset_w,
-                 &render.buf[i * (render.cols + 1)], render.cols);
-    }
-    mvwprintw(ascii_win, 1, 1, "pos: %dx%d size: %dwx%dh", ascii_x, ascii_y,
-              ascii_w, ascii_h);
-    wrefresh(ascii_win);
-
-    mvwprintw(input_win, 1, 1, "%s", input.buf);
-    wrefresh(input_win);
-    ch = getch();
-  } while (true);
-
-  free(render.buf);
-  stbi_image_free(img.img);
-  endwin();
+  teardown(&state);
 }
