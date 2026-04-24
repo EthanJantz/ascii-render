733c8fc6840e2095b572f9d0fb5ea64fb85826ed
EthanJantz <21095848+EthanJantz@users.noreply.github.com>
2026-04-23 20:34:51 -0500

    fix sampling approach and iterate only over extracted channels
    I had been off-by-one on my sampling approach and had unintentionally
    been iterating over the image channels instead of only the channels I
    had pulled from the image for rendering. This fixes that.

--- ascii-renderer-nc.c
+++ ascii-renderer-nc.c
@@ -60,7 +60,8 @@ void update_input(UserInput *cur, int in);
 char ASCII[10] = {' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'};
 int N_ASCII = sizeof(ASCII) / sizeof(ASCII[0]);
 int BLOCK_ASPECT_RATIO = 2;
-int SAMPLES = 1;
+int SAMPLES = 4;
+int CHANNELS = 3;
 
 void setup_ncurses() {
   srand(time(0));
@@ -92,18 +93,19 @@ AppState init_state() {
 
   WINDOW *input_win = newwin(input_h, input_w, input_y, input_x);
   wborder(input_win, 0, 0, 0, 0, 0, 0, 0, 0);
-  mvwprintw(input_win, 1, 1, "pos: %dx%d size: %dwx%dh", input_x, input_y,
-            input_w, input_h);
 
   WINDOW *ascii_win = newwin(ascii_h, ascii_w, ascii_y, ascii_x);
   wborder(ascii_win, 0, 0, 0, 0, 0, 0, 0, 0);
-  mvwprintw(ascii_win, 1, 1, "pos: %dx%d size: %dwx%dh", ascii_x, ascii_y,
-            ascii_w, ascii_h);
+
+  ASCIIRender r = {0};
+  UserInput in = {0};
 
   AppState state = {.term_w = sx,
                     .term_h = sy,
                     .input_win = input_win,
                     .ascii_win = ascii_win,
+                    .cur_render = r,
+                    .user_input = in,
                     .should_rerender = true,
                     .should_quit = false};
 
@@ -121,6 +123,8 @@ void update(AppState *state, int event) {
     if (state->should_rerender && state->user_input.len > 0) {
       int err = read_img(state->user_input.buf, &state->img);
       if (err) {
+        werase(state->ascii_win);
+        wborder(state->ascii_win, 0, 0, 0, 0, 0, 0, 0, 0);
         mvwprintw(state->ascii_win, 2, 1, "Could not load image\n");
         return;
       }
@@ -137,6 +141,8 @@ void update(AppState *state, int event) {
       if (bw == 0)
         bw = 1;
 
+      if (state->cur_render.buf != 0)
+        free(state->cur_render.buf);
       state->cur_render = convert_to_ascii(&state->img, bw, SAMPLES);
     }
     return;
@@ -224,7 +230,8 @@ void reinit_windows(AppState *state) {
 }
 
 int read_img(char *filename, Image *img) {
-  img->img = stbi_load(filename, &img->width, &img->height, &img->channels, 3);
+  img->img =
+      stbi_load(filename, &img->width, &img->height, &img->channels, CHANNELS);
   if (img->img == NULL) {
     return 1;
   }
@@ -264,12 +271,12 @@ ASCIIRender convert_to_ascii(Image *img, int block_width, int samples) {
 
   for (int row = 0; row < rows; row++) {
     for (int col = 0; col < cols; col++) {
-      for (int ch = 0; ch < img->channels; ch++) {
+      for (int ch = 0; ch < CHANNELS; ch++) {
         avg_rel_lum = 0.0;
 
         for (int s = 1; s <= samples; s++) {
-          int x = col * block_width + block_width / s;
-          int y = row * block_height + block_height / s;
+          int x = col * block_width + (s * block_width) / (s + 1);
+          int y = row * block_height + (s * block_height) / (s + 1);
 
           if (ch == 0) {
             r = img->img[(y * img->width + x) * 3 + ch];
